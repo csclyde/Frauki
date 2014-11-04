86ab2c0fc310c1585f71e9cbcf4e84a1c246ab0d
Casey Clyde <caseysclyde@gmail.com>
2014-11-04 13:50:07 -0800

    weapon controller skeleton

---
+++ Src/WeaponController.js
@@ -0,0 +1,67 @@
+WeaponController = function() {
+  this.currentWeapon = null;
+};
+
+WeaponController.prototype.create = function() {
+  
+};
+
+WeaponController.prototype.Update = function() {
+  
+};
+
+WeaponController.prototype.Bomb = {
+    Start: function() {
+        //the initial activity when you press the button
+    },
+    
+    Update: function() {
+        //what to do while updating (only called while active)
+    },
+    
+    Stop: function() {
+        //the final activity when they release the button
+    }
+};
+
+WeaponController.prototype.Mace = {
+    Start: function() {
+        //the initial activity when you press the button
+    },
+    
+    Update: function() {
+        //what to do while updating (only called while active)
+    },
+    
+    Stop: function() {
+        //the final activity when they release the button
+    }
+};
+
+WeaponController.prototype.Bubble = {
+    Start: function() {
+        //the initial activity when you press the button
+    },
+    
+    Update: function() {
+        //what to do while updating (only called while active)
+    },
+    
+    Stop: function() {
+        //the final activity when they release the button
+    }
+};
+
+WeaponController.prototype.Jumper = {
+    Start: function() {
+        //the initial activity when you press the button
+    },
+    
+    Update: function() {
+        //what to do while updating (only called while active)
+    },
+    
+    Stop: function() {
+        //the final activity when they release the button
+    }
+};
