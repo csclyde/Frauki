0fa9f5b1df1c99fe023b35a0f95f2f421a3f599d
Casey Clyde <caseysclyde@gmail.com>
2014-11-14 16:06:14 -0800

    Update Insectoid.js

--- Src/Enemies/Insectoid.js
+++ Src/Enemies/Insectoid.js
@@ -11,6 +11,7 @@ Enemy.prototype.types['Insectoid'] =  function() {
     this.attackTimer = 0;
     this.weight = 0.6;
     this.damage = 5;
+    this.energy = 200;
 
     this.squashTween = null;
 
