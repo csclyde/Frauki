481b847a12c4bdbb0bfb12ed560e869823a11951
Casey Clyde <caseysclyde@gmail.com>
2014-11-20 11:57:38 -0800

    Update EnergyController.js

--- Src/EnergyController.js
+++ Src/EnergyController.js
@@ -26,7 +26,7 @@ EnergyController.prototype.UpdateEnergy = function() {
 	//more perturbed it is. 
 
 	var energyDiff = this.energy - this.neutralPoint;
-	var step = 1.2;//-1 * energyDiff / 10;
+	var step = 0.65;//-1 * energyDiff / 10;
 
 	/*if(step < 0.005 && step > -0.005) this.energy = this.neutralPoint;
 	else if(step < 0.2 && step > 0) step = 0.2;
@@ -44,7 +44,7 @@ EnergyController.prototype.UpdateEnergy = function() {
 			}
 		}
 		
-		this.tickTimer = game.time.now + 200;
+		this.tickTimer = game.time.now + 100;
 	}
 
 	//clamp the enrgy and neutral point;
