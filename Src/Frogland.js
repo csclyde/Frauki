77b1a596e4b354d03324e474dfa3f3a653173830
Casey Clyde <caseysclyde@gmail.com>
2014-12-02 16:13:26 -0800

    Update Frogland.js

--- Src/Frogland.js
+++ Src/Frogland.js
@@ -55,13 +55,14 @@ Frogland.create = function() {
 
     game.add.plugin(Phaser.Plugin.Debug);
 
-    game.canvas.style['display'] = 'none';
+    /*game.canvas.style['display'] = 'none';
     pixel.canvas = Phaser.Canvas.create(game.width * pixel.scale, game.height * pixel.scale);
     pixel.context = pixel.canvas.getContext('2d');
     Phaser.Canvas.addToDOM(pixel.canvas);
     Phaser.Canvas.setSmoothingEnabled(pixel.context, false);
     pixel.width = pixel.canvas.width;
-    pixel.height = pixel.canvas.height;
+    pixel.height = pixel.canvas.height;*/
+    game.scale.setGameSize(512 * 3, 288 * 3);
 
 	game.physics.startSystem(Phaser.Physics.ARCADE);
     game.physics.arcade.gravity.y = 800;
