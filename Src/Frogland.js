090097189871102766af0144df92fa9fd61007cf
Casey Clyde <caseysclyde@gmail.com>
2014-12-10 17:03:37 -0800

    Update Frogland.js

--- Src/Frogland.js
+++ Src/Frogland.js
@@ -65,8 +65,6 @@ Frogland.create = function() {
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.physics.arcade.gravity.y = 800;
-    
-    game.physics.arcade.TILE_BIAS = 40;
 
     bg = game.add.tileSprite(0, 0, 512, 288, 'Background');
     bg.fixedToCamera = true;
