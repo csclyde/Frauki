17d7a66d80ee0c84b38889ecb214ac4b3960b09b
Casey Clyde <caseysclyde@gmail.com>
2015-06-03 12:31:35 -0700

    Update Frauki.js

--- Src/Frauki.js
+++ Src/Frauki.js
@@ -544,7 +544,7 @@ Player.prototype.Rolling = function() {
         //this.body.acceleration.x = this.movement.rollDirection * 1800 * -1 * game.math.catmullRomInterpolation([0, 0.7, 1, 1, 0.7, 0], dur);
         //this.body.acceleration.x += accelMod;
         this.body.acceleration.x = 0;
-        this.body.drag.x = 1200;
+        this.body.drag.x = 1500 * game.math.catmullRomInterpolation([1, 0.4, 0.1, 0.1, 0.4, 1], dur);
     }
     
     if(this.body.velocity.y < 0) {
