bbadbba23f5531406a39f0467c34cb15676ca68f
Casey Clyde <caseysclyde@gmail.com>
2015-06-07 16:24:02 -0700

    added trigger controller skeleton

---
+++ Src/TriggerController.js
@@ -0,0 +1,13 @@
+TriggerController = function() {
+
+  this.timers = new TimerUtil();
+  
+};
+
+TriggerController.prototype.create = function(map) {
+  
+};
+
+TriggerController.prototype.Update = function() {
+    
+};
