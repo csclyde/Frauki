8060b621de5517e7416c13e9113d2d4c008708ba
Casey Clyde <caseysclyde@gmail.com>
2015-06-08 13:43:22 -0700

    Update Music.js

--- Src/Triggers/Music.js
+++ Src/Triggers/Music.js
@@ -1,12 +1,13 @@
-TriggerController.prototype.triggers = {
-
-	'music_surface_ruins': {
-		enter: function(params) { events.publish('stop_music', { name: 'Surface' } ); },
-		exit: function(params) { events.publish('play_music', { name: 'Ruins' } ); }
+TriggerController.prototype.triggers['music_surface_ruins'] = {
+	enter: function(params) {
+		events.publish('stop_music', { name: 'Surface' } );
 	},
-	
-	'music_ruins_junkyard': {
-	    enter: function(params) { events.publish('stop_music', { name: 'Ruins' } ); },
-		exit: function(params) { events.publish('play_music', { name: 'Junkyard' } ); }
+
+	stay: function(params) {
+
+	},
+
+	exit: function(params) {
+		events.publish('play_music', { name: 'Ruins' } );
 	}
 }
