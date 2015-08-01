5dc8c8d7539624c3498f6dee8c9c78092f7c9c3b
Casey Clyde <caseysclyde@gmail.com>
2015-08-01 00:47:47 -0700

    camera fix

--- Src/Main.js
+++ Src/Main.js
@@ -48,9 +48,11 @@ Main.render = function() {
         game.debug.body(o);
     });*/
 
+console.log(pixel.scale);
+
     pixel.context.drawImage(game.canvas, 0, 0, game.width, game.height, 
-        -(pixel.width) - cameraController.camX, 
-        -(pixel.height) - cameraController.camY, 
+        -(game.width * pixel.what) - cameraController.camX, 
+        -(game.height * pixel.what) - cameraController.camY, 
         pixel.width * pixel.scale, 
         pixel.height * pixel.scale);
 };
