c6169d30cb612c93d685a2412eccf40f7bcd9120
Casey Clyde <caseysclyde@gmail.com>
2015-08-03 03:36:20 -0700

    various

--- Src/Enemies/KR32.js
+++ Src/Enemies/KR32.js
@@ -1,10 +1,12 @@
 Enemy.prototype.types['KR32'] =  function() {
 
-	this.body.setSize(15, 60, 0, 0);
+	this.body.setSize(15, 60, 0, -68);
 	this.anchor.setTo(.5, 1);
 
     this.animations.add('idle', ['KR32/Stand0000'], 10, true, false);
-    this.animations.add('shit', ['Hop0000'], 10, true, false);
+    this.animations.add('block', ['KR32/Block0000'], 10, true, false);
+    this.animations.add('windup', ['KR32/Attack0000'], 5,  false, false);
+    this.animations.add('attack', ['KR32/Attack0001', 'KR32/Attack0002', 'KR32/Attack0003', 'KR32/Attack0004', 'KR32/Attack0005', 'KR32/Attack0006', 'KR32/Attack0007', 'KR32/Attack0008'], 18, false, false);
 
     /*
     this.weight = 0.5;
@@ -17,6 +19,9 @@ Enemy.prototype.types['KR32'] =  function() {
 	this.updateFunction = function() {
 
 	};
+
+	Enemy.prototype.CanCauseDamage = function() { return false; }
+	Enemy.prototype.CanChangeDirection = function() { return false; }
 
 	///////////////////////////////ACTIONS////////////////////////////////////
 
@@ -35,12 +40,59 @@ Enemy.prototype.types['KR32'] =  function() {
         this.state = this.Idling;
     };
 
+    this.Attack = function() {
+    	if(!this.timers.TimerUp('attack')) {
+    		return;
+    	}
+
+    	this.state = this.Windup;
+
+    	this.timers.SetTimer('attack', 1000);
+    };
+
 	////////////////////////////////STATES////////////////////////////////////
 	this.Idling = function() {
 		this.PlayAnim('idle');
 
-		
+		if(this.PlayerIsVisible()) {
+			this.state = this.Blocking;
+		}
 	};
+
+	this.Blocking = function() {
+		this.PlayAnim('block');
+
+		if(frauki.body.center.x < this.body.center.x) {
+			this.SetDirection('left');
+		} else {
+			this.SetDirection('right');
+		}
+
+		if(!this.PlayerIsVisible()) {
+			this.state = this.Idling;
+		}
+
+		if(this.PlayerDistance() < 60) {
+			this.Attack();
+		}
+
+	};
+
+	this.Windup = function() {
+		this.PlayAnim('windup');
+
+		if(this.animations.currentAnim.isFinished) {
+			this.state = this.Attacking;
+		}
+	}
+
+	this.Attacking = function() {
+		this.PlayAnim('attack');
+
+		if(this.animations.currentAnim.isFinished) {
+			this.state = this.Blocking;
+		}
+	}
 
 	this.Hurting = function() {
 		this.PlayAnim('die');
