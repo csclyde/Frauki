85068a5ef5c9a459fd632d5242302e1a65b5bb7d
Casey Clyde <caseysclyde@gmail.com>
2015-08-21 11:01:52 -0700

    vombat

--- Src/Enemies/A3PZ.js
+++ Src/Enemies/A3PZ.js
@@ -5,11 +5,11 @@ Enemy.prototype.types['A3PZ'] =  function() {
 
     this.animations.add('idle', ['A3PZ/Stand0000'], 10, true, false);
     this.animations.add('walk', ['A3PZ/Walk0000', 'A3PZ/Walk0001', 'A3PZ/Walk0002', 'A3PZ/Walk0003', 'A3PZ/Walk0004', 'A3PZ/Walk0005'], 8, true, false);
-    this.animations.add('block', ['A3PZ/Block0000', 'A3PZ/Block0000', 'A3PZ/Block0000', 'A3PZ/Block0000', 'A3PZ/Block0000', 'A3PZ/Block0000'], 10, false, false);
+    this.animations.add('block', ['A3PZ/Block0000'], 18, false, false);
     this.animations.add('windup1', ['A3PZ/Attack0001', 'A3PZ/Attack0002'], 10, false, false);
-    this.animations.add('attack1', ['A3PZ/Attack0003', 'A3PZ/Attack0004', 'A3PZ/Attack0005', 'A3PZ/Attack0006', 'A3PZ/Attack0007'], 18, false, false);
+    this.animations.add('attack1', ['A3PZ/Attack0003', 'A3PZ/Attack0004', 'A3PZ/Attack0005', 'A3PZ/Attack0006', 'A3PZ/Attack0007'], 22, false, false);
     this.animations.add('windup2', ['A3PZ/Attack0008', 'A3PZ/Attack0009'], 10, false, false);
-    this.animations.add('attack2', ['A3PZ/Attack0010', 'A3PZ/Attack0011', 'A3PZ/Attack0012', 'A3PZ/Attack0013', 'A3PZ/Attack0014'], 14, false, false);
+    this.animations.add('attack2', ['A3PZ/Attack0010', 'A3PZ/Attack0011', 'A3PZ/Attack0012', 'A3PZ/Attack0013', 'A3PZ/Attack0014'], 24, false, false);
     this.animations.add('hurt', ['A3PZ/Hurt0000', 'A3PZ/Hurt0001'], 8, true, false);
 
     this.energy = 4;
@@ -74,7 +74,7 @@ Enemy.prototype.types['A3PZ'] =  function() {
 
     	this.state = this.Dodging;
 
-    	this.timers.SetTimer('dodge', 750);
+    	this.timers.SetTimer('dodge_hold', 100);
     }
 
 	////////////////////////////////STATES////////////////////////////////////
@@ -160,8 +160,16 @@ Enemy.prototype.types['A3PZ'] =  function() {
 	this.Dodging = function() {
 		this.PlayAnim('block');
 
-		if(this.animations.currentAnim.isFinished) {
-			this.state = this.Idling;
+		console.log('blocking');
+
+		if(this.timers.TimerUp('dodge_hold')) {
+			if(this.PlayerDistance() < 200) {
+				this.Attack();
+			} else {
+				this.state = this.Idling;
+			}
+
+			this.timers.SetTimer('dodge', 750);
 		}
 	}
 
