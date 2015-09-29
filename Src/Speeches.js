3e409c7d782a35f7b65e2d86ac09435ac45d7d6b
Casey Clyde <caseysclyde@gmail.com>
2015-09-28 21:51:34 -0700

    speechy stuffs

--- Src/Speeches.js
+++ Src/Speeches.js
@@ -5,5 +5,28 @@ var Speeches = {
 
 	'tomatoes': 'Mmm, what juicy little apples... or are they tomatoes?',
 
-	'shade_tree': 'This looks like a good spot for a nap...'
+	'shade_tree': 'This looks like a good spot for a nap...',
+
+	'pillar': 'This looks dangerous...',
+
+	'jerk': 'Sneaky jerk',
+
+	'mushrooms': 'These mushrooms don’t look safe to eat... I better not',
+
+	'wet_clay': 'It’s damp in this room, and it smells like wet clay',
+
+	'dew': 'These flowers are covered in dew',
+
+	'draft': 'I can’t see anything out the window, but I can feel a breeze coming through',
+
+	'dripping': 'Eee, the water is dripping on me',
+
+	'fresh': 'The air smells fresh in this room',
+
+	'extract': 'It looks like they were extracting something here',
+
+	'spores': 'The spores are making it hard to breathe',
+
+	'delicate_flower': 'This flower looks delicate... I wonder how long it’s been growing'
+
 };
