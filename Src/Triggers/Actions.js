462dbe267e0d7b1e93c8ff866c323b144ac700cc
Casey Clyde <caseysclyde@gmail.com>
2016-01-18 19:45:18 -0800

    stuff

---
+++ Src/Triggers/Actions.js
@@ -0,0 +1,13 @@
+
+TriggerController.prototype.triggers['open_door'] = {
+	enter: function(params) {
+
+	},
+
+	stay: function(params) {
+		//if there are no enemies within the region
+	},
+
+	exit: function(params) {
+	}
+}
